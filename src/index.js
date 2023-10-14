42ad5834632d808ccb2e717e55a621e6499a4d38
root <+>
2023-10-14 11:35:40 +0300

    assignment complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+    addToDo();
+  });
+  form.reset();
 });
+
+function addToDo() {
+  const input = document.getElementById("new-task-description");
+  const todo = input.value;
+  if (todo) {
+    let li = document.createElement("li");
+    let btn = document.createElement("button");
+    btn.addEventListener("click", handleDelete);
+    btn.textContent = "x";
+    li.textContent = todo;
+    li.appendChild(btn);
+    document.querySelector("#tasks").appendChild(li);
+    input.value = ""; // Clear the input field after adding the task
+  }
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
